f189546d1fa87e276c714d2b5a57ac6426206adb
barualfian789 <alfiankurada@gmail.com>
2025-02-08 10:43:37 +0700

    Add files via upload

---
+++ ad-format.js
@@ -0,0 +1,9 @@
+
+	atOptions = {
+		'key' : '6f962b70ca9f849983ad0fb455cceeca',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="//www.highperformanceformat.com/6f962b70ca9f849983ad0fb455cceeca/invoke.js"></scr' + 'ipt>');
